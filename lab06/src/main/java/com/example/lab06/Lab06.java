4ff94f01347fa0358d4b0a0e09269998c57fbee8
seodong-lim <seodong.lim@ontariotechu.net>
2022-03-16 16:23:55 -0400

    lab06

---
+++ lab06/src/main/java/com/example/lab06/Lab06.java
@@ -0,0 +1,27 @@
+package com.example.lab06;
+
+import javafx.scene.paint.Color;
+
+public class Lab06 {
+
+    private static double[] avgHousingPricesByYear = {
+            247381.0,264171.4,287715.3,294736.1,
+            308431.4,322635.9,340253.0,363153.7
+    };
+    private static double[] avgCommercialPricesByYear = {
+            1121585.3,1219479.5,1246354.2,1295364.8,
+            1335932.6,1472362.0,1583521.9,1613246.3
+    };
+
+    private static String[] ageGroups = {
+            "18-25", "26-35", "36-45", "46-55", "56-65", "65+"
+    };
+    private static int[] purchasesByAgeGroup = {
+            648, 1021, 2453, 3173, 1868, 2247
+    };
+    private static Color[] pieColours = {
+            Color.AQUA, Color.GOLD, Color.DARKORANGE,
+            Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM
+    };
+
+}
